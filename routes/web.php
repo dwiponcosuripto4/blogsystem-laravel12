2c82726910b243638e6a8f7eea774f7245b82497
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2025-11-27 06:54:06 +0700

    Halaman Blog Post

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,25 @@ Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog Page']);
+Route::get('/posts', function () {
+    $posts = [
+        [
+            "title" => "Judul Artikel 1",
+            "author" => "Dwiponco Suripto",
+            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis sunt ipsam
+            doloribus laboriosam cupiditate
+            earum nemo, possimus modi. Expedita veritatis a tenetur eveniet error culpa dolorum laborum, minus sit
+            alias."
+        ],
+        [
+            "title" => "Judul Artikel 2",
+            "author" => "Jane Doe",
+            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid fugit veniam
+            reprehenderit omnis. Enim placeat asperiores odio labore amet eius molestiae fugit accusamus perferendis,
+            pariatur excepturi repudiandae quo. Aperiam, mollitia."
+        ]
+    ];
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
 Route::get('/about', function () {

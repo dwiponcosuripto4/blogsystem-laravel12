6ced5a28edad89b24ed39389d3ea09cb104947e5
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2025-11-27 07:54:33 +0700

    Halaman Single Blog Post

---
+++ resources/views/post.blade.php
@@ -0,0 +1,13 @@
+<x-layout :title="$title">
+    <article class="py-8 max-w-3xl">
+
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
+
+        <div class="text-base text-gray-500">
+            <a href="#">{{ $post['author'] }}</a> | 12 Agustus 2023
+        </div>
+        <p class="my-4 font-light">{{ $post['body'] }}</p>
+        <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to all
+            posts.</a>
+    </article>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,6 +10,8 @@ Route::get('/', function () {
 Route::get('/posts', function () {
     $posts = [
         [
+            "id" => 1,
+            "slug" => "judul-artikel-1",
             "title" => "Judul Artikel 1",
             "author" => "Dwiponco Suripto",
             "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis sunt ipsam
@@ -17,6 +20,8 @@ Route::get('/posts', function () {
             alias."
         ],
         [
+            "id" => 2,
+            "slug" => "judul-artikel-2",
             "title" => "Judul Artikel 2",
             "author" => "Jane Doe",
             "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid fugit veniam
@@ -25,6 +30,38 @@ Route::get('/posts', function () {
         ]
     ];
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            "id" => 1,
+            "slug" => "judul-artikel-1",
+            "title" => "Judul Artikel 1",
+            "author" => "Dwiponco Suripto",
+            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis sunt ipsam
+            doloribus laboriosam cupiditate
+            earum nemo, possimus modi. Expedita veritatis a tenetur eveniet error culpa dolorum laborum, minus sit
+            alias."
+        ],
+        [
+            "id" => 2,
+            "slug" => "judul-artikel-2",
+            "title" => "Judul Artikel 2",
+            "author" => "Jane Doe",
+            "body" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid fugit veniam
+            reprehenderit omnis. Enim placeat asperiores odio labore amet eius molestiae fugit accusamus perferendis,
+            pariatur excepturi repudiandae quo. Aperiam, mollitia."
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    if (!$post) abort(404);
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/about', function () {
